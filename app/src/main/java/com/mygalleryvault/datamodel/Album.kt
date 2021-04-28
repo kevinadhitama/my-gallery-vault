4c081c50db3843d28b2ee7c6ce7d913680d53d1f
Kevin Adhitama <vinzadhitama@gmail.com>
2021-04-28 17:28:42 +0700

    ref: add parcelize impl

--- app/src/main/java/com/mygalleryvault/datamodel/Album.kt
+++ app/src/main/java/com/mygalleryvault/datamodel/Album.kt
@@ -1,11 +1,15 @@
 package com.mygalleryvault.datamodel
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
 /**
  * Create by kevin.adhitama pm 4/26/2021.
  */
+@Parcelize
 data class Album(
     val id: String,
     val name: String,
     val tsCreated: String,
     val listImage: MutableList<Picture>
-)
+) : Parcelable
--- app/src/main/java/com/mygalleryvault/datamodel/Picture.kt
+++ app/src/main/java/com/mygalleryvault/datamodel/Picture.kt
@@ -1,10 +1,14 @@
 package com.mygalleryvault.datamodel
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
 /**
  * Create by kevin.adhitama pm 4/26/2021.
  */
+@Parcelize
 data class Picture(
     val filePath: String,
     val fileName: String,
     val tsCreated: String
-)
+) : Parcelable
