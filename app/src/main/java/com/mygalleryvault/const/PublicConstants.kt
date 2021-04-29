492e3ebf671a8b2bb88ae9b3e4befa4d411b567b
Kevin Adhitama <vinzadhitama@gmail.com>
2021-04-29 12:22:28 +0700

    feat: add uses permission

---
+++ app/src/main/java/com/mygalleryvault/const/PublicConstants.kt
@@ -0,0 +1,8 @@
+package com.mygalleryvault.const
+
+/**
+ * Create by kevin.adhitama pm 4/28/2021.
+ */
+object PublicConstants {
+    const val fileProviderPath = "com.mygalleryvault.fileProvider"
+}
