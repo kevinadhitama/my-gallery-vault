fc18adf7e302c741008d61919055bb93689a6e7e
Kevin Adhitama <vinzadhitama@gmail.com>
2021-04-30 16:21:36 +0700

    ref: remove unused file and fields

--- app/src/main/java/com/mygalleryvault/page/MainActivity.kt
+++ app/src/main/java/com/mygalleryvault/page/MainActivity.kt
@@ -64,17 +64,11 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_albums, R.id.nav_gallery, R.id.nav_settings
+                R.id.nav_albums, R.id.nav_settings
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main, menu)
-        return true
     }
 
     override fun onSupportNavigateUp(): Boolean {
---
+++ app/src/main/java/com/mygalleryvault/page/ui/setting/SettingFragment.kt
@@ -0,0 +1,23 @@
+package com.mygalleryvault.page.ui.setting
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.mygalleryvault.R
+
+class SettingFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.fragment_setting, container, false)
+        val textView: TextView = root.findViewById(R.id.text_setting)
+        textView.text = "Upcoming Feature"
+        return root
+    }
+}
